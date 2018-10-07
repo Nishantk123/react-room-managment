0060b261f60c9beec8281e6b65bea205574b8716
Nishant kashyap <sannukashyap24@gmail.com>
2018-10-07 18:11:06 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import RemoveIcon from '@material-ui/icons/RemoveCircle';
 import AddIcon from '@material-ui/icons/AddCircle';
-import GroupIcon from '@material-ui/icons/Group';
-
+import PersonIcon from '@material-ui/icons/Person';
+import AccessibilityIcon from '@material-ui/icons/AccessibilityNew';
+import  group from'./group.png';
+import roomIcon from './room.png';
 class App extends Component {
   constructor(props){
     super(props);
@@ -13,6 +15,7 @@ class App extends Component {
       child:0
     };
   }
+// for remove the room count
   removeRoomCount = () => {
     let room = this.state.room;
     let totalMember = this.state.child + this.state.adult;
@@ -23,6 +26,7 @@ class App extends Component {
        this.setState({room:room});
     }
   }
+  // for add a room count
   addRoomCount = () => {
     let room = this.state.room;
     let adult = this.state.adult;
@@ -32,6 +36,7 @@ class App extends Component {
       this.setState({room:room, adult:adult});
     }
   }
+  //for add adult count
   addAdultCount = () => {
     let adult = this.state.adult;
     let availableCount = 4 * this.state.room;
@@ -41,16 +46,16 @@ class App extends Component {
       this.setState({adult:adult});
     }
   }
+  // for remove adult count
   removeAdultCount = () => {
     let adult = this.state.adult;
     let child = this.state.child;
-    let room = this.state.room;
-    if(child==0 && adult>1){
+    if(child ===0 && adult>1){
       adult -=1
       this.setState({adult:adult});
     }
-
   }
+  //for add child count
   addChildCount = () => {
     let child = this.state.child;
     let availableCount = 4 * this.state.room;
@@ -60,11 +65,9 @@ class App extends Component {
       this.setState({child:child});
     }
   }
+  //for remove child count
   removeChildCount  = () => {
     let child = this.state.child;
-    let availableCount = 4 * this.state.room;
-    let totalMember = this.state.child + this.state.adult;
-    // let prevoiusRoom = 4 * (room-1);
     if(child>0)
     child -=1;
     this.setState({child:child})
@@ -73,62 +76,66 @@ class App extends Component {
     return (
       <div className="App">
         <div className="room-detail-label">
-          <GroupIcon
-          className="group-icon"
-          />
-         <span className="room-label">Choose Number of</span> <span className="room-people-label">People</span>
-          </div>
+          <img  src={group} alt ="group" />
+         <div className="room-label">Choose Number of <span className="room-people-label">People</span></div>
+        </div>
        <div className="room-container">
         <div className="room-count-detail">
           <div className="left-side-detail">
-          <AddIcon /> <span className="room-name">ROOMS</span>
+          <img  src={roomIcon} alt ="group" />
+           <div className="room-name">ROOMS</div>
           </div>
           <div className="right-side-detail">
             <RemoveIcon
             onClick={(e) => this.removeRoomCount(e)}
+            className="remove-icon"
             />
-            <span className="room-count">{this.state.room}</span>
+            <div className="room-count">{this.state.room}</div>
             <AddIcon 
             onClick={(e) => this.addRoomCount(e)}
+            className="add-icon"
             />
           </div>
         
         </div>
         <div className="adults-count-detail">
           <div className="left-side-detail">
-          <AddIcon /> <span className="room-name">ADULTS</span>
+          <PersonIcon /> 
+         
+          <span className="room-name">ADULTS</span>
           </div>
           <div className="right-side-detail">
             <RemoveIcon
              onClick={(e) => this.removeAdultCount(e)}
+             className="remove-icon"
             />
-            <span className="room-count">{this.state.adult}</span>
+            <div className="room-count">{this.state.adult}</div>
             <AddIcon
              onClick={(e) => this.addAdultCount(e)}
+             className="add-icon"
             />
           </div>
         
         </div>
         <div className="child-count-detail">
           <div className="left-side-detail">
-          <AddIcon /> <span className="room-name">CHILDREN</span>
+          <AccessibilityIcon /> <span className="child-room-name">CHILDREN</span>
           </div>
           <div className="right-side-detail">
             <RemoveIcon
              onClick={(e) => this.removeChildCount(e)}
+             className="remove-icon"
             />
             <span className="room-count">{this.state.child}</span>
             <AddIcon 
              onClick={(e) => this.addChildCount(e)}
+             className="add-icon"
             />
-          </div>
-        
+          </div> 
         </div>
-       
        </div>
       </div>
     );
   }
 }
-
 export default App;
